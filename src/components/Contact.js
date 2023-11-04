79708c546adc86c28d884f4eae5fd39bf82149f0
david solomon <david.solomon.henshaw@gmail.com>
2023-11-04 01:52:54 +0100

    added .loading spinner in contact from component for when form is submitted using bootstrap

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,87 +1,8 @@
-// import React , { useRef } from 'react';
-// import emailjs from '@emailjs/browser';
 
-// const Contact = () => {
-
-//   const form = useRef();
-
-
-//   const handleSubmit = (e) => {
-//      e.preventDefault();
-
-//     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-//       .then((result) => {
-//           console.log(result.text);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//   };
- 
-//   return (
-//     <div className="container form " style={{marginBottom: "190px", marginTop: "200px"}}>
-//       <h2 className="text-center">Contact Us</h2>
-//       <div className="d-flex justify-content-center">
-//         <div className="col-md-6 col-12">
-//           <form ref={form} onSubmit={handleSubmit}>
-//             <div className="mb-3">
-//               <label htmlFor="name" className="form-label">
-//                 Name
-//               </label>
-//               <input
-//                 type="text"
-//                 className="form-control"
-//                 id="name"
-//                 name="name"
-//                 placeholder="Your Name"
-//                 required
-//               />
-//             </div>
-//             <div className="mb-3">
-//               <label htmlFor="email" className="form-label">
-//                 Email
-//               </label>
-//               <input
-//                 type="email"
-//                 className="form-control"
-//                 id="email"
-//                 name="email"
-//                 placeholder="Your Email"
-//                 required
-//               />
-//             </div>
-//             <div className="mb-3">
-//               <label htmlFor="message" className="form-label">
-//                 Message
-//               </label>
-//               <textarea
-//                 className="form-control"
-//                 id="message"
-//                 name="message"
-//                 rows="5"
-//                 placeholder="Your Message"
-//                 required
-//               ></textarea>
-//             </div>
-//             <button style={{ width: '100%' }} type="submit" className="btn btn-success">
-//               Submit
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
-
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-// const serviceId = 'service_9qmafbm'
-// const templateId = 'template_7fqwobr';
 
 const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
 const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
@@ -89,11 +10,12 @@ const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
 
 const Contact = () => {
   const form = useRef();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('clicked')
-    //console.log(serviceId,templateId,publicKey)
+    setIsLoading(true);
+
     emailjs
       .sendForm(serviceId, templateId, form.current, publicKey)
       .then((result) => {
@@ -104,11 +26,14 @@ const Contact = () => {
       .catch((error) => {
         console.error(error.text);
         toast.error('Error sending message');
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
   return (
-    <div className="container form" style={{ marginBottom: "190px", marginTop: "200px" }}>
+    <div className="container form" style={{ marginBottom: '190px', marginTop: '200px' }}>
       <h2 className="text-center">Contact Us</h2>
       <div className="d-flex justify-content-center">
         <div className="col-md-6 col-12">
@@ -155,6 +80,15 @@ const Contact = () => {
             <button style={{ width: '100%' }} type="submit" className="btn btn-success">
               Submit
             </button>
+            {isLoading && (
+              <div className="overlay">
+                <div className="d-flex justify-content-center">
+                  <div className="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                </div>
+              </div>
+            )}
           </form>
         </div>
       </div>
